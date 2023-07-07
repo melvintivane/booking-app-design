ffb1bedcbb44ff6409e95dc75a8dafdd4fafdf6c
Mello47 <melvinshuster47@gmail.com>
2023-07-07 15:53:01 +0200

    added Navbar and Header components

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Navbar from "../../components/Navbar/Navbar";
+import Header from "../../components/Header/Header";
 
 const List = () => {
   return (
-    <div>List</div>
+    <div>
+      <Navbar />
+      <Header type="list"/>
+    </div>
   )
 }
 
