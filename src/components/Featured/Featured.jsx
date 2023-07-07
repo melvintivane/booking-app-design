3be65b4b92a3bfb95b04ea5a7b31a74e99322acf
Mello47 <melvinshuster47@gmail.com>
2023-07-07 15:57:32 +0200

    created featured component

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import "./Featured.css"
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className='featuredContainer'>
+      <div className="featuredItem">
+        <img className='featuredImg' src="https://images.pexels.com/photos/3021382/pexels-photo-3021382.jpeg?auto=compress&cs=tinysrgb&w=1080" alt="" />
+        <div className="featuredTitles">
+          <h1>Roma</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div> 
+      <div className="featuredItem">
+        <img className='featuredImg' src="https://images.pexels.com/photos/236294/pexels-photo-236294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="featuredTitles">
+          <h1>Moscow</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div> 
+      <div className="featuredItem">
+        <img className='featuredImg' src="https://images.pexels.com/photos/15606856/pexels-photo-15606856/free-photo-of-kandy-town-queens-hotel.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="featuredTitles">
+          <h1>Cape Town</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div> 
+      <div className="featuredItem">
+        <img className='featuredImg' src="https://images.pexels.com/photos/1770812/pexels-photo-1770812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="featuredTitles">
+          <h1>Brooklyn</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div> 
+    </div>
   )
 }
 
