d04ec78dce92926cf1be5dfd0a1b592fd70f8ad7
Mello47 <melvinshuster47@gmail.com>
2023-07-07 15:50:27 +0200

    added Navbar, Header, Featured and PropertyList components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css"
+import Navbar from "../../components/Navbar/Navbar";
+import Header from "../../components/Header/Header";
+import Featured from '../../components/Featured/Featured';
+import PropertyList from '../../components/PropertyList/PropertyList';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar/>
+      <Header/>
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+      </div>
+    </div>
   )
 }
 
