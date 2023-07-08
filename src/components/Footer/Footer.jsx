726288470ab1ebd99f69cf9f8c0180ffc15fd2d0
Mello47 <melvinshuster47@gmail.com>
2023-07-08 20:55:55 +0200

    created Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import "./Footer.css"
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<div className="footer">
+			<div className="footerLists">
+				<ul className="footerList">
+					<li className="footerListItem">Countries</li>
+					<li className="footerListItem">Regions</li>
+					<li className="footerListItem">Cities</li>
+					<li className="footerListItem">Districts</li>
+					<li className="footerListItem">Airports</li>
+					<li className="footerListItem">Hotels</li>
+				</ul>
+				<ul className="footerList">
+					<li className="footerListItem">Homes </li>
+					<li className="footerListItem">Apartments </li>
+					<li className="footerListItem">Resorts </li>
+					<li className="footerListItem">Villas</li>
+					<li className="footerListItem">Hostels</li>
+					<li className="footerListItem">Guest houses</li>
+				</ul>
+				<ul className="footerList">
+					<li className="footerListItem">Unique places to stay </li>
+					<li className="footerListItem">Reviews</li>
+					<li className="footerListItem">Unpacked: Travel articles </li>
+					<li className="footerListItem">Travel communities </li>
+					<li className="footerListItem">Seasonal and holiday deals </li>
+				</ul>
+				<ul className="footerList">
+					<li className="footerListItem">Car rental </li>
+					<li className="footerListItem">Flight Finder</li>
+					<li className="footerListItem">Restaurant reservations </li>
+					<li className="footerListItem">Travel Agents </li>
+				</ul>
+				<ul className="footerList">
+					<li className="footerListItem">Curtomer Service</li>
+					<li className="footerListItem">Partner Help</li>
+					<li className="footerListItem">Careers</li>
+					<li className="footerListItem">Sustainability</li>
+					<li className="footerListItem">Press center</li>
+					<li className="footerListItem">Safety Resource Center</li>
+					<li className="footerListItem">Investor relations</li>
+					<li className="footerListItem">Terms & conditions</li>
+				</ul>
+			</div>
+			<div className="footerText">Copyright © 2023 Melvinbooking.</div>
+    	</div>
+	)
 }
 
 export default Footer
