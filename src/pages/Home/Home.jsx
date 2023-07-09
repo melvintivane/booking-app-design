9877db8eee5028b6db48554e1f0860d5c49bce4e
Mello47 <melvinshuster47@gmail.com>
2023-07-09 19:57:25 +0200

    added MailList and Footer components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,20 +5,24 @@ import Header from "../../components/Header/Header";
 import Featured from '../../components/Featured/Featured';
 import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties';
 import PropertyList from '../../components/PropertyList/PropertyList';
+import MailList from '../../components/MailList/MailList';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar/>
-      <Header/>
-      <div className="homeContainer">
-        <Featured />
-        <h1 className="homeTitle">Browse by property type</h1>
-        <PropertyList />
-        <FeaturedProperties />
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<Navbar/>
+			<Header/>
+			<div className="homeContainer">
+				<Featured />
+				<h1 className="homeTitle">Browse by property type</h1>
+				<PropertyList />
+				<FeaturedProperties />
+				<MailList />
+				<Footer />
+			</div>
+		</div>
+	)
 }
 
 export default Home
