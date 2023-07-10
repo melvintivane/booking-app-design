94793d0523217178dbab8e167563ceddfda76bdc
Mello47 <melvinshuster47@gmail.com>
2023-07-10 12:56:28 +0200

    added h1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,6 +17,7 @@ const Home = () => {
 				<Featured />
 				<h1 className="homeTitle">Browse by property type</h1>
 				<PropertyList />
+				<h1 className="homeTitle">Homes guests love</h1>
 				<FeaturedProperties />
 				<MailList />
 				<Footer />
