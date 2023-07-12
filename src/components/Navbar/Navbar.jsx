81e3789820992922ff38c8d079d12382840875b0
Mello47 <melvinshuster47@gmail.com>
2023-07-12 18:30:30 +0200

    added Link to Navbar items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import "./Navbar.css"
+import { Link } from 'react-router-dom';
+import languageImg from "../../assets/mzn-flag.png";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbarContainer">
-        <span className="logo">Melvinbooking</span>
-        <div>
+        <Link className='link' to="/">
+          <span className="logo">Melvinbooking</span>
+        </Link>
+        <div className='right'>
+          <div className="navbarRightItem">
+            <span className='currency'>MZN</span>
+          </div>
+          <div className="navbarRightItem">
+            <div className="imgContainer">
+              <img src={languageImg} alt="" className="languageImg" />
+            </div>
+          </div>
+          <div className="navbarRightItem">
+            <div className="help">
+              <span>?</span>
+            </div>
+          </div>
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
         </div>
