1cb92574b8df7a99acb22729b16d9f0cc1c691ac
Mello47 <melvinshuster47@gmail.com>
2023-07-12 18:34:39 +0200

    added Attractions type

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { format } from 'date-fns';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = ({type}) => {
 	const [destination, setDestination] = useState("");
@@ -54,34 +54,43 @@ const Header = ({type}) => {
 			<div className={type === "list" ? "headerContainer listMode" : "headerContainer"}>
 				<div className="headerList">
 					<div className="headerListItem active">
-						<FontAwesomeIcon icon={faBed} />
-						<span>Stays</span>
+						<Link className='link'>
+							<FontAwesomeIcon className='icon' icon={faBed} />
+							<span>Stays</span>
+						</Link>
 					</div>
 					<div className="headerListItem">
-						<FontAwesomeIcon icon={faPlane} />
-						<span>Flights</span>
+						<Link className='link'>
+							<FontAwesomeIcon className='icon' icon={faPlane} />
+							<span>Flights</span>
+						</Link>
 					</div>
 					<div className="headerListItem">
-						<FontAwesomeIcon icon={faCar} />
-						<span>Car rentals</span>
+						<Link className='link'>
+							<FontAwesomeIcon className='icon' icon={faCar} />
+							<span>Car rentals</span>
+						</Link>
 					</div>
 					<div className="headerListItem">
-						<FontAwesomeIcon icon={faMagnet} />
-						<span>Attractions</span>
+						<Link className='link' to="/attractions">
+							<FontAwesomeIcon className='icon' icon={faMagnet} />
+							<span>Attractions</span>
+						</Link>
 					</div>
 					<div className="headerListItem">
-						<FontAwesomeIcon icon={faTaxi} />
-						<span>Airport taxis</span>
+						<Link className='link'>
+							<FontAwesomeIcon className='icon' icon={faTaxi} />
+							<span>Airport taxis</span>
+						</Link>
 					</div>
 					</div>
-					{ type !== "list" && <><h1 className="headerTitle">
-					A lifetime of discounts? It's Genius.
+					{ type == "home" && <>
+					<h1 className="headerTitle">
+						Find your next stay
 					</h1>
 					<p className="headerDesc">
-						Get rewarded for your travels – unlock instant savings of 10% or
-						more with a free Lamabooking account
+						Search low prices on hotels, homes and much more...
 					</p>
-					<button className="headerBtn">Sign in / Register</button>
 					<div className="headerSearch">
 						<div className="headerSearchItem">
 							<FontAwesomeIcon icon={faBed} className='headerIcon' />
@@ -132,6 +141,34 @@ const Header = ({type}) => {
 									<button className='doneBtn'>Done</button>
 								</div>
 							</div>}
+						</div>
+						<div className="headerSearchItem">
+						<button className="headerBtn" onClick={handleSearch}>Search</button>
+					</div>
+				</div></>}
+					{ type == "attractions" && <>
+					<h1 className="headerTitle">
+						Attractions, activities and experiences
+					</h1>
+					<p className="headerDesc">
+						Discover new attractions and experiences to match your interests and travel style
+					</p>
+					<div className="headerSearch">
+						<div className="headerSearchItem">
+							<FontAwesomeIcon icon={faBed} className='headerIcon' />
+							<input type="text" placeholder='Where are you going?' className='headerSearchInput' onChange={ (e)=> setDestination(e.target.value) }/>
+						</div>						
+						<div className="headerSearchItem">
+							<FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+							<span onClick={handleOpenDate} className='headerSearchText'>{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(date[0].endDate, "dd/MM/yyyy")}`}</span>
+							{openDate && <DateRange
+								editableDateInputs={true}
+								onChange={item => setDate([item.selection])}
+								moveRangeOnFirstSelection={false}
+								ranges={date}
+								className='date'
+								minDate={new Date()}
+							/>}
 						</div>
 						<div className="headerSearchItem">
 						<button className="headerBtn" onClick={handleSearch}>Search</button>
