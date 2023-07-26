28001c8f1d397057077431e434f28931edb5fc06
Mello47 <melvinshuster47@gmail.com>
2023-07-26 19:43:06 +0200

    ADD function to map Attractions Items

--- src/pages/Attractions/Attractions.jsx
+++ src/pages/Attractions/Attractions.jsx
@@ -6,6 +6,48 @@ import Footer from '../../components/Footer/Footer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarCheck, faHeadset, faMagnet } from '@fortawesome/free-solid-svg-icons';
 
+
+
+const data = [
+  {
+    id: 1,
+    title: "Dubai",
+    desc: "793 things to do",
+    img: "https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72203688.jpg?k=882dfd9985340ffb2a8553b56580d2139455c38e310899b3949b6dff9a8590df&o="
+  },
+  {
+    id: 2,
+    title: "London",
+    desc: "793 things to do",
+    img: "https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204269.jpg?k=dda35f09d4db949aa6786bc25686bddd016cdd12a2603193e90df0d44a51a1ac&o="
+  },
+  {
+    id: 3,
+    title: "Instambul",
+    desc: "793 things to do",
+    img: "https://q-xx.bstatic.com/xdata/images/city/633x400/654667.jpg?k=38b7cadbc436ac8f1990d593aeea431bdca024ad7e19136de1a648a741efd1d2&o="
+  },
+  {
+    id: 4,
+    title: "New York",
+    desc: "793 things to do",
+    img: "https://q-xx.bstatic.com/xdata/images/city/633x400/976784.jpg?k=717a6a83ea61edb06017bb8c9bd3d36511ec0e1aef59ac94235584d4fd1709cb&o="
+  },  
+  {
+    id: 5,
+    title: "Las Vegas",
+    desc: "391 things to do",
+    img: "https://q-xx.bstatic.com/xdata/images/city/633x400/977068.jpg?k=60e8c40ecdd20063f9c429d6e3837cb35f972446db199ddc23a67f28a01c6c68&o="
+  },  
+  {
+    id: 6,
+    title: "Paris",
+    desc: "670 things to do",
+    img: "https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204347.jpg?k=2028e72e4ea4eb18da986b8a60fd841f65fe0575db5012d723233b4427939b4a&o="
+  },  
+]
+
+
 const Attractions = () => {
   return (
     <div>
@@ -15,48 +57,15 @@ const Attractions = () => {
         <div className="attractionsWrapper">
           <h1 className='attractionsTitle'>Top destinations</h1>
           <div className="attractionsList">
-            <div className="attractionsItem">
-              <img className="attractionsItemImg" src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72203688.jpg?k=882dfd9985340ffb2a8553b56580d2139455c38e310899b3949b6dff9a8590df&o=" alt="" srcset="" />
-              <div className="attractionsTitles">
-                <h4>Dubai</h4>
-                <span>793 things to do</span>
-              </div>
-            </div>
-            <div className="attractionsItem">
-              <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204269.jpg?k=dda35f09d4db949aa6786bc25686bddd016cdd12a2603193e90df0d44a51a1ac&o=" alt="" srcset="" />
-              <div className="attractionsTitles">
-                <h4>London</h4>
-                <span>711 things to do</span>
-              </div>
-            </div>
-            <div className="attractionsItem">
-              <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/654667.jpg?k=38b7cadbc436ac8f1990d593aeea431bdca024ad7e19136de1a648a741efd1d2&o=" alt="" srcset="" />
-              <div className="attractionsTitles">
-                <h4>Instambul</h4>
-                <span>213 things to do</span>
-              </div>
-            </div>
-            <div className="attractionsItem">
-              <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/976784.jpg?k=717a6a83ea61edb06017bb8c9bd3d36511ec0e1aef59ac94235584d4fd1709cb&o=" alt="" srcset="" />
-              <div className="attractionsTitles">
-                <h4>New York</h4>
-                <span>691 things to do</span>
-              </div>
-            </div>
-            <div className="attractionsItem">
-              <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/977068.jpg?k=60e8c40ecdd20063f9c429d6e3837cb35f972446db199ddc23a67f28a01c6c68&o=" alt="" srcset="" />
-              <div className="attractionsTitles">
-                <h4>Las Vegas</h4>
-                <span>391 things to do</span>
-              </div>
-            </div>
-            <div className="attractionsItem">
-              <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204347.jpg?k=2028e72e4ea4eb18da986b8a60fd841f65fe0575db5012d723233b4427939b4a&o=" alt="" srcset="" />
-              <div className="attractionsTitles">
-                  <h4>Paris</h4>
-                  <span>670 things to do</span>
-              </div>
-            </div>
+            {data.map((item) => (
+                <div className="attractionsItem" key={item.id}>
+                  <img className='attractionsItemImg' src={item.img} alt=""/>
+                  <div className="attractionsTitles">
+                      <h4>{item.title}</h4>
+                      <span>{item.desc}</span>
+                  </div>
+                </div>
+            ))}
           </div>
         </div>
         <div className="attractionsSignIn">
@@ -109,132 +118,33 @@ const Attractions = () => {
           <hr />
           <div className="attractionsWrapper">
             <div className="attractionsListTeste">
-              <div className="attractionsItemTeste">
-                <img className="attractionsItemImg" src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72203688.jpg?k=882dfd9985340ffb2a8553b56580d2139455c38e310899b3949b6dff9a8590df&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>Dubai</h4>
-                  <span>793 things to do</span>
+              {data.map((item) => (
+                <div className="attractionsItemTeste" key={item.id}>
+                  <img className="attractionsItemImg" src={item.img} alt="" />
+                  <div className="attractionsTitles">
+                    <h4>{item.title}</h4>
+                    <span>{item.desc}</span>
+                  </div>
                 </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204269.jpg?k=dda35f09d4db949aa6786bc25686bddd016cdd12a2603193e90df0d44a51a1ac&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>London</h4>
-                  <span>711 things to do</span>
+              ))}
+              {data.map((item) => (
+                <div className="attractionsItemTeste" key={item.id}>
+                  <img className="attractionsItemImg" src={item.img} alt="" />
+                  <div className="attractionsTitles">
+                    <h4>{item.title}</h4>
+                    <span>{item.desc}</span>
+                  </div>
                 </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/654667.jpg?k=38b7cadbc436ac8f1990d593aeea431bdca024ad7e19136de1a648a741efd1d2&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>Instambul</h4>
-                  <span>213 things to do</span>
+              ))}
+              {data.map((item) => (
+                <div className="attractionsItemTeste" key={item.id}>
+                  <img className="attractionsItemImg" src={item.img} alt="" />
+                  <div className="attractionsTitles">
+                    <h4>{item.title}</h4>
+                    <span>{item.desc}</span>
+                  </div>
                 </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/976784.jpg?k=717a6a83ea61edb06017bb8c9bd3d36511ec0e1aef59ac94235584d4fd1709cb&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>New York</h4>
-                  <span>691 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/977068.jpg?k=60e8c40ecdd20063f9c429d6e3837cb35f972446db199ddc23a67f28a01c6c68&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>Las Vegas</h4>
-                  <span>391 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204347.jpg?k=2028e72e4ea4eb18da986b8a60fd841f65fe0575db5012d723233b4427939b4a&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                    <h4>Paris</h4>
-                    <span>670 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className="attractionsItemImg" src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72203688.jpg?k=882dfd9985340ffb2a8553b56580d2139455c38e310899b3949b6dff9a8590df&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>Dubai</h4>
-                  <span>793 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204269.jpg?k=dda35f09d4db949aa6786bc25686bddd016cdd12a2603193e90df0d44a51a1ac&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>London</h4>
-                  <span>711 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/654667.jpg?k=38b7cadbc436ac8f1990d593aeea431bdca024ad7e19136de1a648a741efd1d2&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>Instambul</h4>
-                  <span>213 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/976784.jpg?k=717a6a83ea61edb06017bb8c9bd3d36511ec0e1aef59ac94235584d4fd1709cb&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>New York</h4>
-                  <span>691 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/977068.jpg?k=60e8c40ecdd20063f9c429d6e3837cb35f972446db199ddc23a67f28a01c6c68&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>Las Vegas</h4>
-                  <span>391 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204347.jpg?k=2028e72e4ea4eb18da986b8a60fd841f65fe0575db5012d723233b4427939b4a&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                    <h4>Paris</h4>
-                    <span>670 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className="attractionsItemImg" src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72203688.jpg?k=882dfd9985340ffb2a8553b56580d2139455c38e310899b3949b6dff9a8590df&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>Dubai</h4>
-                  <span>793 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204269.jpg?k=dda35f09d4db949aa6786bc25686bddd016cdd12a2603193e90df0d44a51a1ac&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>London</h4>
-                  <span>711 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/654667.jpg?k=38b7cadbc436ac8f1990d593aeea431bdca024ad7e19136de1a648a741efd1d2&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>Instambul</h4>
-                  <span>213 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/976784.jpg?k=717a6a83ea61edb06017bb8c9bd3d36511ec0e1aef59ac94235584d4fd1709cb&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>New York</h4>
-                  <span>691 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/city/633x400/977068.jpg?k=60e8c40ecdd20063f9c429d6e3837cb35f972446db199ddc23a67f28a01c6c68&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                  <h4>Las Vegas</h4>
-                  <span>391 things to do</span>
-                </div>
-              </div>
-              <div className="attractionsItemTeste">
-                <img className='attractionsItemImg' src="https://q-xx.bstatic.com/xdata/images/xphoto/633x400/72204347.jpg?k=2028e72e4ea4eb18da986b8a60fd841f65fe0575db5012d723233b4427939b4a&o=" alt="" srcset="" />
-                <div className="attractionsTitles">
-                    <h4>Paris</h4>
-                    <span>670 things to do</span>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
